4e9c9b7b9f2c3da043d7f3100097d57827962d35
aditya <adityakulkarni54321@gmail.com>
2025-08-03 23:50:56 +0530

    changes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,14 +1,9 @@
-import tailwindcss from 'tailwindcss'; // Use ES Module import syntax
-
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["picsum.photos", "example.com"],
   },
-  
-  // Add the plugins array
-  plugins: [tailwindcss],
 };
 
-export default nextConfig; // Use ES Module export syntax
+export default nextConfig;
