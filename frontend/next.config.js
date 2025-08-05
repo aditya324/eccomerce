c4981707297c1cdd60a4fc418677ce0cfda7224a
aditya <adityakulkarni54321@gmail.com>
2025-08-05 16:18:40 +0530

    changes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,17 @@
-/** @type {import("next").NextConfig} */
+import path from "path";
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
+
   images: {
     unoptimized: true,
-    domains: ["picsum.photos", "example.com","eccomerce-vuc3.onrender.com"],
+    domains: ["picsum.photos", "example.com", "eccomerce-vuc3.onrender.com"],
+  },
+
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve('./');
+    return config;
   },
 };
 
