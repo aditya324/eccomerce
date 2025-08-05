c68028985b80573e8171b2d3c30da372c3d07b5b
aditya <adityakulkarni54321@gmail.com>
2025-08-05 13:24:23 +0530

    changes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -87,8 +87,8 @@ export const loginUser = async (req, res) => {
  
     res.cookie("jwt", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+       secure: true,
+      sameSite: "none",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
